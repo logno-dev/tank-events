f27d01ab014388fd80f9a32cb560249248b040f4
Logan Bunch <loganbunch@gmail.com>
2024-10-29 15:35:47 -0700

    Tue Oct 29 15:35:47 PDT 2024

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useMemo } from "react";
 import { v4 as uuid } from "uuid";
 
 export default function Cell({ data, day, item }) {
+  const [edit, setEdit] = useState(false);
   const items = useMemo(() => {
     const d = day.fullDate.toISOString().split(".")[0].split("T")[0];
     const arr = data.filter((i) => {
@@ -16,20 +18,35 @@ export default function Cell({ data, day, item }) {
         {items.map((i) => {
           const hour = Number(i.date.split("T")[1].split(":")[0]);
           return (
-            <div
+            <button
               className={
-                "absolute w-full h-[10px] text-[8px] flex justify-center timeline-" +
+                "absolute w-full h-[10px] text-[8px] cursor-pointer flex justify-center timeline-" +
                 hour +
                 " " +
                 i.type
               }
               key={i.id + i.type + i.item + uuid()}
+              onClick={() => setEdit(true)}
             >
               {i.type}
-            </div>
+            </button>
           );
         })}
+        {edit && <Edit setEdit={setEdit} />}
       </div>
+    </>
+  );
+}
+
+function Edit(i, setEdit) {
+  return (
+    <>
+      <button
+        onClick={() => setEdit(false)}
+        className="fixed flex place-items-center place-content-center top-0 left-0 right-0 bottom-0 bg-[#77777755]"
+      >
+        <div className="p-3 bg-gray-500">TEST</div>
+      </button>
     </>
   );
 }
